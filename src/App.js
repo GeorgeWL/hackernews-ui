02a9d8e2ba9a37e5af920da3edfe1b5c5af78bf8
George W Langham <georgelangham1990@gmail.com>
2021-05-04 19:34:58 +0100

    change to using SCSS. create basic fetch functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import "./styles.css";
-
+import "./styles.scss";
+import { useEffect, useState } from "react";
+const BASE_API_URL = "https://hacker-news.firebaseio.com/v0";
+async function getPosts() {
+  const url = BASE_API_URL + "/topstories.json";
+  const topStoryIDs = (await (await fetch(url)).json()).slice(0, 10);
+  const topStoriesWithDetails = [];
+  topStoryIDs.forEach(async (storyID) => {
+    const data = await getPostById(storyID);
+    topStoriesWithDetails.push(data);
+  });
+  return topStoriesWithDetails;
+}
+async function getPostById(postID) {
+  const url = `${BASE_API_URL}/item/${postID}.json`;
+  const data = await (await fetch(url)).json();
+  return data;
+}
+const fetchItems = async () => await getPosts();
 export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(async () => {
+    setLoading(false);
+    // temp mock before switch to using proper
+    const items = fetchItems();
+    setPosts(items);
+  }, 4000);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>HackerNews</h1>
+      <h2>Top 10 Current Posts</h2>
+      <ul className="posts">
+        {loading
+          ? "Loading"
+          : posts?.length > 0
+          ? posts.map((post) => <li key={`post-${post.id}`}>{post}</li>)
+          : "No Posts loaded"}
+      </ul>
     </div>
   );
 }
